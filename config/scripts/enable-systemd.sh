b6e2a3de3b023958c5c1e9d56584c0c3c1085285
Nadav Spiegelman <me@nadav.name>
2023-11-18 14:45:56 -0500

    Use script to enable automount

---
+++ config/scripts/enable-systemd.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -euxo pipefail
+systemctl enable var-mnt-docs.automount
